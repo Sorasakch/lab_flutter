d9a023597836a1abd9773d98567d3aaa04e07e22
Sorasakch <Sorsak.char@gmail.com>
2024-03-30 15:21:10 +0700

    lab

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'My App : รหัสนักศึกษา'),
+      home: const MyHomePage(title: 'My App : 66130413'),
     );
   }
 }
